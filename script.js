8654aedcb749b235ce018ca600a77d7645d2938d
admin <danisanchez86@gmail.com>
2020-11-18 12:21:01 +0100

    Check button taking the guess number

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const number = Math.trunc(Math.random()*20)+1;
+
+let guess = document.querySelector('.check').addEventListener('click', function(){
+    guess = Number(document.querySelector('.guess').value);
+    // console.log(typeof guess);
+
+    
+});
+
